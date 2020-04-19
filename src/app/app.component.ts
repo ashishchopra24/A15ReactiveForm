b9e2d9b06183ce22a90973c791dc8957150115c9
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2020-04-19 11:19:33 +0530

    CustomValidator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +9,25 @@ import {FormControl, FormGroup} from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['test', 'demo'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      username: new FormControl(null),
-      email: new FormControl(null,),
+      username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+      email: new FormControl(null, [Validators.required, Validators.email] ),
       gender: new FormControl('male')
     });
   }
 
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {nameIsForbidden: true};
+    }
+    return null;
+  }
+
 
   onSubmit() {
-    console.log(this.signupForm);
+     console.log(this.signupForm);
   }
 }
